72848794e8668ea44bc857439b40298b3cdfcfde
Domingos chiconela <azevedodomingos78@gmail.com>
2024-10-03 17:46:40 +0200

    feat:created  taskController

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -11,8 +11,10 @@ import { fromZodError } from "zod-validation-error"
 const taskSchema  = z.object({
 
     title :z.string(), 
-  description:z.string(),
-  due_date:z.string(),
+    description:z.string(),
+    due_date: z.date().refine(date => date > new Date(), {
+    message: "A data de vencimento deve ser uma data futura", 
+  }),
 
 })
 
@@ -21,6 +23,35 @@ const updateTaskSchema = taskSchema.partial();
 
 export const createTask = async (req: Request, res: Response) => {
 
+    const {userId}= req
+
+  try {
     
 
+      const validation = taskSchema.safeParse(req.body);
+      if (!validation.success) {
+          return res.status(400).json({ message: fromZodError(validation.error).details })
+      }
+
+      const newPost = await db.task.create({
+          data: {
+              title:validation.data.title,
+              description:validation.data.description,
+              due_date:validation.data.due_date,
+              userId
+          }
+          
+      })
+      
+
+      res.status(201).json({ message: "Post created", data: newPost })
+  } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: "Internal Server Error" });
+
+  }
+
+
 }
+
+
